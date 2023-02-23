5a8b9d7274d78fb89f5ab278c587a68ba7ea0db1
Fredy Sajan <f60984157@gmail.com>
2023-02-23 22:10:34 +0530

    Updates

---
+++ precache-manifest.b6d048aef4f8679f39221832f9df36e7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c6209f637935a92df16e",
+    "url": "/Pinocchio-Clothing-Store-/static/css/main.37e327c6.chunk.css"
+  },
+  {
+    "revision": "c6209f637935a92df16e",
+    "url": "/Pinocchio-Clothing-Store-/static/js/main.c6209f63.chunk.js"
+  },
+  {
+    "revision": "8654e7e83f475fb519e8",
+    "url": "/Pinocchio-Clothing-Store-/static/js/1.8654e7e8.chunk.js"
+  },
+  {
+    "revision": "51e3efacc161d9526a76",
+    "url": "/Pinocchio-Clothing-Store-/static/js/runtime~main.51e3efac.js"
+  },
+  {
+    "revision": "b1f9f2ecd536a47fd7f65c7e1dbd013b",
+    "url": "/Pinocchio-Clothing-Store-/index.html"
+  }
+];
