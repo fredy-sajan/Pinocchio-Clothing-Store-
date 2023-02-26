665ebf425b315f5e8ce7a2cfb7ebbbfae2821f2a
Fredy Sajan <f60984157@gmail.com>
2023-02-27 01:07:21 +0530

    Popular product's component added in home page ( mobile version design )

---
+++ src/features/ui/components/home-components/popular-product-component/index.js
@@ -0,0 +1,40 @@
+import React from 'react'
+
+// CSS import
+import './stylesheet/style.css'
+
+// import assets
+import nextIcon from '../../../../../assets/svg/home-section/popular-products-section/next-icon.svg'
+import previousIcon from '../../../../../assets/svg/home-section/popular-products-section/previous-icon.svg'
+
+function PopularProductsComponent() {
+    return (
+        <>
+            <div className="popular-products-container">
+                <div className="popular-products-contents">
+                    <div className="popular-products-head-text">
+                        <h1>`Popular Product's</h1>
+                    </div>
+                    <div className="popular-products-cards">
+                        <div className="card">
+
+                        </div>
+                    </div>
+                    <div className="next-previous">
+                        <div className="next">
+                            <img src={previousIcon} alt="" />
+                        </div>
+                        <div className="previous">
+                            <img src={nextIcon} alt="" />
+                        </div>
+                        <div className="see-all">
+                            <p>See all</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default PopularProductsComponent
