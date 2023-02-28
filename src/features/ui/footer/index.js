1e1db0c9a1acb255f5d2cf92fc61c4c576bb6413
Fredy Sajan <f60984157@gmail.com>
2023-03-01 02:11:11 +0530

    PC design of 'Footer section' design completed

--- src/features/ui/footer/index.js
+++ src/features/ui/footer/index.js
@@ -44,41 +44,50 @@ function Footer() {
 
                 <div className="under-svg">
                     <div class="custom-shape-divider-top-1677526532">
-                        <svg  data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+                        <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
                             <path d="M600,112.77C268.63,112.77,0,65.52,0,7.23V120H1200V7.23C1200,65.52,931.37,112.77,600,112.77Z" class="shape-fill"></path>
                         </svg>
                     </div>
                 </div>
 
                 <div className="footer-links">
-                    <div className="first-line">
-                        <ul>
-                            <li>Home</li>
-                            <li>About</li>
-                            <li>Contact</li>
-                        </ul>
-                    </div>
-                    <div className="second-line">
-                        <ul>
-                            <li>Offers</li>
-                            <li>Products</li>
-                            <li>New Arrivals</li>
-                        </ul>
+
+                    <div className="main-footer-links">
+
+                        <div className="first-line">
+                            <ul>
+                                <li>Home</li>
+                                <li>About</li>
+                                <li>Contact</li>
+                            </ul>
+                        </div>
+                        <div className="second-line">
+                            <ul>
+                                <li>Offers</li>
+                                <li>Products</li>
+                                <li>New Arrivals</li>
+                            </ul>
+                        </div>
+
                     </div>
 
-                    <div className="second-important-links">
-                        <div className="first-important-line">
-                            <ul>
-                                <li>Latest launch</li>
-                                <li>Popular product's</li>
-                            </ul>
+                    <div className="second-main-footer-links">
+
+                        <div className="second-important-links">
+                            <div className="first-important-line">
+                                <ul>
+                                    <li>Latest launch</li>
+                                    <li>Popular product's</li>
+                                </ul>
+                            </div>
+                            <div className="second-important-line">
+                                <ul>
+                                    <li>Customer’s review</li>
+                                    <li>Brand’s we provide</li>
+                                </ul>
+                            </div>
                         </div>
-                        <div className="second-important-line">
-                            <ul>
-                                <li>Customer’s review</li>
-                                <li>Brand’s we provide</li>
-                            </ul>
-                        </div>
+
                     </div>
 
                     <div className="copyright">
