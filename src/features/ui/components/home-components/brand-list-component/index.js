042b0aa0eee1f260c5a8a9a1e48c3520dcd63feb
Fredy Sajan <f60984157@gmail.com>
2023-02-28 18:08:58 +0530

    in home component, brandlist component pc design completed

--- src/features/ui/components/home-components/brand-list-component/index.js
+++ src/features/ui/components/home-components/brand-list-component/index.js
@@ -6,19 +6,32 @@ import firstBrandLineNames from '../../../../../assets/svg/home-section/brands-s
 import secondBrandLineNames from '../../../../../assets/svg/home-section/brands-section/secondbrands.svg'
 import thirdBrandLineNames from '../../../../../assets/svg/home-section/brands-section/thirdbrands.svg'
 
+import firstBrandLineNamesForPcScreen from '../../../../../assets/svg/home-section/brands-section/firstbrands-pc-screen.svg'
+import secondBrandLineNamesForPcScreen from '../../../../../assets/svg/home-section/brands-section/secondbrands-pc-screen.svg'
+import thirdBrandLineNamesForPcScreen from '../../../../../assets/svg/home-section/brands-section/thirdbrands-pc-screen.svg'
+
+
+
 function BrandsInside() {
   return (
     <>
-        <div className="brands-inside-section">
-            <div className="brands-inside-text-head">
-                <h1>Brand's Inside</h1>
-            </div>
-            <div className="brands-inside-names">
-                <img src={firstBrandLineNames} alt="" />
-                <img src={secondBrandLineNames} alt="" />
-                <img src={thirdBrandLineNames} alt="" />
-            </div>
+      <div className="brands-inside-section">
+        <div className="brands-inside-text-head">
+          <h1>Brand's Inside</h1>
         </div>
+        <div className="brands-inside-names">
+          <div className="mobile-screen">
+            <img src={firstBrandLineNames} alt="" />
+            <img src={secondBrandLineNames} alt="" />
+            <img src={thirdBrandLineNames} alt="" />
+          </div>
+          <div className="pc-screen">
+            <img src={firstBrandLineNamesForPcScreen} alt="" />
+            <img src={secondBrandLineNamesForPcScreen} alt="" />
+            <img src={thirdBrandLineNamesForPcScreen} alt="" />
+          </div>
+        </div>
+      </div>
     </>
   )
 }
